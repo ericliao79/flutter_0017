761bfbab333799ed267c700ce7e236a1ffd990ee
Eric <liaogd79@gmail.com>
2022-05-16 17:48:17 +0800

    💡 feat: update image

--- lib/model.dart
+++ lib/model.dart
@@ -18,17 +18,17 @@ class Hero {
 List heroes = [
   Hero(
     color: Color(0xFF86F3FB),
-    image: "https://game.gtimg.cn/images/lol/act/img/skin/big22009.jpg",
-    title: '寒冰射手-艾希',
+    image: "https://images.unsplash.com/photo-1652636373308-31e97c10f6ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1615&q=80",
+    title: '圖一',
   ),
   Hero(
     color: Color(0xFF7D6588),
-    image: "https://game.gtimg.cn/images/lol/act/img/skin/big39006.jpg",
-    title: '刀鋒舞者-艾瑞莉婭',
+    image: "https://images.unsplash.com/photo-1652529629322-a0019cc0907e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: '圖二',
   ),
   Hero(
     color: Color(0xFF4C314D),
-    image: "https://game.gtimg.cn/images/lol/act/img/skin/big103015.jpg",
-    title: '九尾妖狐-阿狸',
+    image: "https://images.unsplash.com/photo-1652631631001-78de1939bee8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: '圖三',
   ),
 ];
